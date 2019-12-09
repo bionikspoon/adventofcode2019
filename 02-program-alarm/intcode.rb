546ab50ae8d756c344890cb38a945f9a0fcd7b8b
Manu Phatak <manu.phatak@procore.com>
2019-12-09 14:21:08 -0800

    solve day 2

--- 02-program-alarm/intcode.rb
+++ 02-program-alarm/intcode.rb
@@ -1,40 +1,148 @@
 # frozen_string_literal: true
 
 class Intcode
-  attr_reader :state
-  def initialize(state)
-    @state = state
+  attr_reader :memory
+  attr_reader :pointer
+  attr_writer :pointer
+
+  def initialize(memory, noun: nil, verb: nil)
+    @memory = memory
     @pointer = 0
+
+    self.noun = noun
+    self.verb = verb
   end
 
   def run(cycles = Float::INFINITY)
-    return self if cycles.zero?
+    return self if cycles.zero? || pointer.nil?
 
-    case state[pointer]
-    when 1
-      state[state[pointer + 3]] =
-        state[state[pointer + 1]] +
-        state[state[pointer + 2]]
-      @pointer += 4
-
-    when 2
-      state[state[pointer + 3]] =
-        state[state[pointer + 1]] *
-        state[state[pointer + 2]]
-      @pointer += 4
-
-    when 99
-      @pointer += 1
-
-      return self
-    else
-      raise "unknown opcode #{state[pointer]}"
-    end
+    Instruction.run(opcode: memory[pointer], intcode: self)
 
     run(cycles - 1)
   end
 
+  def noun
+    memory[1]
+  end
+
+  def noun=(noun)
+    memory[1] = noun if noun
+  end
+
+  def verb
+    memory[2]
+  end
+
+  def verb=(verb)
+    memory[2] = verb if verb
+  end
+
+  def noun_verb_hash
+    100 * noun + verb
+  end
+
+  class GoalSeek
+    def initialize(noun_range:, verb_range:, goal:, &block)
+      @noun_range = noun_range
+      @verb_range = verb_range
+      @goal = goal
+
+      @create = block
+    end
+
+    def seek
+      @seek ||= begin
+        noun_range.each do |noun|
+          verb_range.each do |verb|
+            subject = create.call noun: noun, verb: verb
+
+            return subject if subject.run.memory.first == goal
+          end
+        end
+
+        raise 'not found'
+      end
+    end
+
+    private
+
+    attr_reader :create, :noun_range, :verb_range, :goal
+  end
+end
+
+class Instruction
+  def self.run(opcode:, intcode:)
+    case opcode
+
+    when 1
+      AddInstruction
+    when 2
+      MultiplyInstruction
+    when 99
+      HaltInstruction
+    else
+      raise "unknown opcode #{memory[pointer]}"
+    end.new(intcode).run
+  end
+
+  def initialize(intcode)
+    @intcode = intcode
+    @params = (1...arity).map { |n| intcode.memory[intcode.pointer + n] }
+  end
+
+  def run
+    set_memory
+    post_run
+  end
+
+  protected
+
+  def post_run
+    intcode.pointer += arity
+  end
+
+  def arity
+    1
+  end
+
+  def operation(current)
+    current
+  end
+
+  def set_memory
+    return unless params.last
+
+    intcode.memory[params.last] =
+      operation(*params.map { |p| intcode.memory[p] })
+  end
+
   private
 
-  attr_reader :pointer
+  attr_reader :intcode, :params
+end
+
+class AddInstruction < Instruction
+  def operation(left, right, _current)
+    left + right
+  end
+
+  def arity
+    4
+  end
+end
+
+class MultiplyInstruction < Instruction
+  def operation(left, right, _current)
+    left * right
+  end
+
+  def arity
+    4
+  end
+end
+
+class HaltInstruction < Instruction
+  def post_run
+    intcode.pointer = nil
+  end
 end
--- 02-program-alarm/intcode_spec.rb
+++ 02-program-alarm/intcode_spec.rb
@@ -5,69 +5,66 @@ require_relative 'intcode'
 RSpec.describe Intcode do
   describe '#run' do
     it 'can be a noop' do
-      subject =  described_class.new([99])
+      intcode =  described_class.new([99])
 
-      expect(subject.run.state).to eq([99])
+      expect(intcode.run.memory).to eq([99])
     end
 
     it 'can add' do
-      subject =  described_class.new([1, 0, 0, 0, 99])
+      intcode =  described_class.new([1, 0, 0, 0, 99])
 
-      expect(subject.run.state).to eq([2, 0, 0, 0, 99])
+      expect(intcode.run.memory).to eq([2, 0, 0, 0, 99])
     end
 
     it 'can multiply' do
-      subject = described_class.new([2, 3, 0, 3, 99])
+      intcode = described_class.new([2, 3, 0, 3, 99])
 
-      expect(subject.run.state).to eq([2, 3, 0, 6, 99])
+      expect(intcode.run.memory).to eq([2, 3, 0, 6, 99])
     end
 
     it 'can stop' do
-      subject = described_class.new([2, 4, 4, 5, 99, 0])
+      intcode = described_class.new([2, 4, 4, 5, 99, 0])
 
-      expect(subject.run.state).to eq([2, 4, 4, 5, 99, 9801])
+      expect(intcode.run.memory).to eq([2, 4, 4, 5, 99, 9801])
     end
 
     it 'can dive into itself' do
-      subject = described_class.new([1, 1, 1, 4, 99, 5, 6, 0, 99])
+      intcode = described_class.new([1, 1, 1, 4, 99, 5, 6, 0, 99])
 
-      expect(subject.run.state).to eq([30, 1, 1, 4, 2, 5, 6, 0, 99])
+      expect(intcode.run.memory).to eq([30, 1, 1, 4, 2, 5, 6, 0, 99])
     end
 
-    it 'can step through state' do
-      subject = described_class.new([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])
+    it 'can step through memory' do
+      intcode = described_class.new([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])
 
-      expect(subject.run(1).state).to eq([1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
-      expect(subject.run(1).state).to eq([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
-      expect(subject.run(1).state).to eq([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
+      aggregate_failures do
+        expect(intcode.run(1).memory).to eq([1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
+        expect(intcode.run(1).memory).to eq([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
+        expect(intcode.run(1).memory).to eq([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
+      end
     end
 
     it 'can run on input.txt' do
       input = File.readlines(File.join(__dir__, 'input.txt')).join(',').chomp.split(',').map(&:to_i)
-      input[1] = 12
-      input[2] = 2
-      subject = described_class.new(input)
 
-      expect(subject.run.state.first).to be(5_305_097)
+      intcode = described_class.new(input, noun: 12, verb: 2)
+
+      expect(intcode.run.memory.first).to be(5_305_097)
     end
+  end
 
-    it 'can iterate through nouns and verbs' do
-      (0..99).each do |noun|
-        (0..99).each do |verb|
-          input = File.readlines(File.join(__dir__, 'input.txt')).join(',').chomp.split(',').map(&:to_i)
-          input[1] = noun
-          input[2] = verb
-          subject = described_class.new(input)
-
-          next unless subject.run.state.first == 19_690_720
-
-          aggregate_failures do
-            expect(noun).to be(49)
-            expect(verb).to be(25)
-
-            expect(100 * noun + verb).to be(4925)
-          end
+  describe Intcode::GoalSeek do
+    describe '#seek' do
+      subject(:goalseek) do
+        described_class.new(noun_range: (0..99), verb_range: (0..99), goal: 19_690_720) do |noun:, verb:|
+          Intcode.new(input.dup, noun: noun, verb: verb)
         end
+      end
+
+      let(:input) { File.readlines(File.join(__dir__, 'input.txt')).join(',').chomp.split(',').map(&:to_i) }
+
+      it 'can iterate through nouns and verbs' do
+        expect(goalseek.seek).to have_attributes(noun: 49, verb: 25, noun_verb_hash: 4925)
       end
     end
   end
